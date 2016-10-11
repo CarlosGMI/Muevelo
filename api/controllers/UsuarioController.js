33597060e54097ce051b6125ad52eeb025a4eec4
CarlosGMI <carlosmaldonado@correo.unimet.edu.ve>
2016-10-11 05:10:48 -0400

    Formulario y conexión

--- api/controllers/UsuarioController.js
+++ api/controllers/UsuarioController.js
@@ -5,7 +5,31 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
-module.exports = {
+module.exports = 
+{
+
+	'Nueva': function(req, res)
+	{
+		res.view();
+	},
 	
+	/*create: function(req, res, next)
+	{
+		usuario.create(req.params.all(), function usuarioCreated(err, usuario)
+		{
+			if (err) return next(err);
+			res.redirect('/usuario/usuarioRegistrado/'+usuario.IDUsuario);
+		});
+	},
+
+	usuarioRegistrado: function(req, res, next)
+	{
+		usuario.findOne(req.param('IDUsuario'), function foundusuario(err, usuario)
+		{
+			if (err) return next(err);
+			if (!usuario) return next();
+			res.view({usuario: usuario});
+		}); //Ojito con el foundusuario --> foundUsuario
+	}*/
 };
 
