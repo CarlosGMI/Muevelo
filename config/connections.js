0cc76ce734842461bfa7597ed1a60905f9bbf3c7
CarlosGMI <carlosmaldonado@correo.unimet.edu.ve>
2016-11-15 22:10:39 -0300

    Cambios menores
    En vez de comentar la mudanza validé si el usuario está logeado para
    acceder.

--- config/connections.js
+++ config/connections.js
@@ -45,9 +45,9 @@ module.exports.connections = {
 
      adapter: 'sails-mysql',
      host: 'localhost',
-     port: 3306,
-     user: 'ramonbrito', 
-     password: 'ramon789Biologia', 
+     port: 3307,
+     user: 'carlosgmi', 
+     password: 'gordito21', 
      database: 'muevelo2' 
   }
 
