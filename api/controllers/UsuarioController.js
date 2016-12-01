120292fee34976f937076b72b74ebd601113dd75
CarlosGMI <carlosmaldonado@correo.unimet.edu.ve>
2016-12-01 08:07:26 -0300

    Un cambio achantado
    No hice el commit de este cambio

--- api/controllers/UsuarioController.js
+++ api/controllers/UsuarioController.js
@@ -16,11 +16,47 @@ module.exports =
 	
 	create: function(req, res, next)
 	{
-		Usuario.create(req.params.all(), function usuarioCreated(err,Usuario)
+		var objetoUsuario = {
+			IDUsuario: req.param('id'),
+			Username: req.param('Username'),
+			Contrasena: req.param('Contrasena'),
+			Nombre: req.param('Nombre'),
+			Apellido: req.param('Apellido'),
+			Cedula: req.param('Cedula'),
+			Telefono: req.param('Telefono'),
+			Correo: req.param('Correo')
+		}
+		var objetoDireccion = {
+			IDDireccion: req.param('id'),
+			Estado: req.param('Estado'),
+			Municipio: req.param('Municipio'),
+			SectorUrb: req.param('SectorUrb'),
+			CodigoPostal: req.param('CodigoPostal'),
+			DetallesDireccion: req.param('DetallesDireccion')
+		}
+		Usuario.create(objetoUsuario, function usuarioCreated(err,Usuario)
 		{
 			if (err) return next(err);
-			res.redirect('/usuario/usuarioRegistrado/'+Usuario.IDUsuario);
-		});	
+			//res.redirect('/usuario/usuarioRegistrado/'+Usuario.IDUsuario);
+		});
+		Direccion.create(objetoDireccion, function direccionCreated(err,Direccion)
+		{
+			if (err) return next(err);
+			Usuario.update({Username: req.param('Username')},{Direcciones: Direccion.IDDireccion}, function direccionUpdated(err,dir)
+			{
+				if (err) return next(err);
+				mandalo(req.param('Username'));
+			});
+		});
+		function mandalo(a) 
+		{
+			Usuario.findOne({Username: a}, function usuarioMandalo(err,user)
+			{
+				if (err) return next(err);
+				if (!Usuario) return next();
+				res.redirect('/usuario/usuarioRegistrado/'+user.IDUsuario);
+			})
+		}
 	},
 
 	usuarioRegistrado: function(req, res, next)
@@ -39,7 +75,12 @@ module.exports =
 		Pedido.query('SELECT * from pedido WHERE pedido.owner = '+userid+';', function(err, results)
 		{
 			if(err) return res.serverError(err);
+			var pedidoid = results.IDPedido;
 			return res.view({Pedido: results});
+			/*Pedido.query('SELECT pedido.IDPedido, servicio.Desmontaje as desmontaje, servicio.Instalacion as instalacion, servicio.Embalaje as embalaje FROM pedido INNER JOIN servicio WHERE pedido.IDPedido = servicio.IDPedidos;', function(err,rr)
+			{
+				return res.view({Pedido: results},{Ped: rr});
+			});*/
 		});
 		/*Usuario.findOne(req.param('id'), function foundUsuario(err,Usuario)
 		{
